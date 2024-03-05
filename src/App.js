713df2e2054e3a146367b98ebed979e1a5055c20
Brett Ossman <brettossman@gmail.com>
2024-03-05 15:10:08 -0500

    Section 3 - Coding Exercise 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import ProfileCard from './ProfileCard';
+//import ProfileCard from './ProfileCard';
+import BrightText from './BrightText';
 
 function App() {
+    // return (
+    //    <div>
+    //        <div>Personal Digital Assistants</div>
+    //        <ProfileCard title = "Alexa" handle = "@alexa99" />
+    //        <ProfileCard title = "Cortana" handle = "@cortana32" />
+    //        <ProfileCard title = "Siri" handle = "@siri01" />
+    //    </div>
+    //);
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title = "Alexa" handle = "@alexa99" />
-            <ProfileCard title = "Cortana" handle = "@cortana32" />
-            <ProfileCard title = "Siri" handle = "@siri01" />
+            <div>Bright Text</div>
+            <BrightText color = "red" />
+            <BrightText color = "green" />
+            <BrightText color = "blue" />
         </div>
     );
 }
