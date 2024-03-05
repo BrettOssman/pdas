2753f0b9d61e382f9644ee1e64fb3d1531bc2c5e
Brett Ossman <brettossman@gmail.com>
2024-03-05 14:54:53 -0500

    Section3 24-29

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,17 @@
-function ProfileCard() {
-    return <div>Profile Card</div>
+//function ProfileCard(props) {
+function ProfileCard({title,handle}) {
+    // const title = props.title;
+    // const handle = props.handle;
+    // can be condensed to:
+    // const { title, handle } = props;
+    // which can be redone as the function parameter, instead of using props as the parameter
+
+    return (
+        <div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </ div>
+    );
 }
 
 export default ProfileCard;
